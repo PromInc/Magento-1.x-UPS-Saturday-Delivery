01c9565dad28e65cc71eae2a5bb32d349481d1d5
Chris Nolan <chris@leytech.co.uk>
2019-01-08 13:29:01 +0000

    Changed order of methods to match Mage_Usa_Model_Shipping_Carrier_Ups

--- app/code/community/Prominc/Shipping/Model/Shipping/Carrier/Ups.php
+++ app/code/community/Prominc/Shipping/Model/Shipping/Carrier/Ups.php
@@ -3,111 +3,6 @@ class Prominc_Shipping_Model_Shipping_Carrier_Ups extends Mage_Usa_Model_Shippin
 {
 
     /**
-     * Prepare shipping rate result based on response
-     *
-     * @param mixed $response
-     * @return Mage_Shipping_Model_Rate_Result
-     */
-    protected function _parseXmlResponse($xmlResponse)
-    {
-        $costArr = array();
-        $priceArr = array();
-        if (strlen(trim($xmlResponse))>0) {
-            $xml = new Varien_Simplexml_Config();
-            $xml->loadString($xmlResponse);
-            $arr = $xml->getXpath("//RatingServiceSelectionResponse/Response/ResponseStatusCode/text()");
-            $success = (int)$arr[0];
-            if ($success===1) {
-                $arr = $xml->getXpath("//RatingServiceSelectionResponse/RatedShipment");
-                $allowedMethods = explode(",", $this->getConfigData('allowed_methods'));
-
-                // Negotiated rates
-                $negotiatedArr = $xml->getXpath("//RatingServiceSelectionResponse/RatedShipment/NegotiatedRates");
-                $negotiatedActive = $this->getConfigFlag('negotiated_active')
-                    && $this->getConfigData('shipper_number')
-                    && !empty($negotiatedArr);
-
-                $allowedCurrencies = Mage::getModel('directory/currency')->getConfigAllowCurrencies();
-
-                foreach ($arr as $shipElement){
-                    $code = (string)$shipElement->Service->Code;
-                    if (in_array($code, $allowedMethods)) {
-
-                        if ($negotiatedActive) {
-                            $cost = $shipElement->NegotiatedRates->NetSummaryCharges->GrandTotal->MonetaryValue;
-                        } else {
-                            $cost = $shipElement->TotalCharges->MonetaryValue;
-                        }
-
-                        //convert price with Origin country currency code to base currency code
-                        $successConversion = true;
-                        $responseCurrencyCode = (string) $shipElement->TotalCharges->CurrencyCode;
-                        if ($responseCurrencyCode) {
-                            if (in_array($responseCurrencyCode, $allowedCurrencies)) {
-                                $cost = (float) $cost * $this->_getBaseCurrencyRate($responseCurrencyCode);
-                            } else {
-                                $errorTitle = Mage::helper('directory')->__('Can\'t convert rate from "%s-%s".', $responseCurrencyCode, $this->_request->getPackageCurrency()->getCode());
-                                $error = Mage::getModel('shipping/rate_result_error');
-                                $error->setCarrier('ups');
-                                $error->setCarrierTitle($this->getConfigData('title'));
-                                $error->setErrorMessage($errorTitle);
-                                $successConversion = false;
-                            }
-                        }
-
-                        if ($successConversion) {
-                            $costArr[$code] = $cost;
-                            $priceArr[$code] = $this->getMethodPrice(floatval($cost),$code);
-                        }
-                    }
-                }
-            } else {
-                $arr = $xml->getXpath("//RatingServiceSelectionResponse/Response/Error/ErrorDescription/text()");
-                $errorTitle = (string)$arr[0][0];
-                $error = Mage::getModel('shipping/rate_result_error');
-                $error->setCarrier('ups');
-                $error->setCarrierTitle($this->getConfigData('title'));
-                $error->setErrorMessage($this->getConfigData('specificerrmsg'));
-            }
-        }
-        $result = Mage::getModel('shipping/rate_result');
-        $defaults = $this->getDefaults();
-        if (empty($priceArr)) {
-            $error = Mage::getModel('shipping/rate_result_error');
-            $error->setCarrier('ups');
-            $error->setCarrierTitle($this->getConfigData('title'));
-            if(!isset($errorTitle)){
-                $errorTitle = Mage::helper('usa')->__('Cannot retrieve shipping rates');
-            }
-            $error->setErrorMessage($this->getConfigData('specificerrmsg'));
-            $result->append($error);
-        } else {
-            if( $this->getSaturdayDelivery() ) {
-                $saturdayShippingMethods = explode( ",", Mage::getStoreConfig('shipping/saturday_delivery_ups/allowed_methods') );
-            }
-            foreach ($priceArr as $method=>$price) {
-                if( $this->getSaturdayDelivery() && !in_array( $method, $saturdayShippingMethods ) ) {
-                    continue;
-                }
-
-                $rate = Mage::getModel('shipping/rate_result_method');
-                $rate->setCarrier('ups');
-                $rate->setCarrierTitle($this->getConfigData('title'));
-                // $rate->setMethod($method);
-                $rate->setMethod( $method . ( $this->getSaturdayDelivery() ? '_sat' : '' ) );
-                // $method_arr = $this->getShipmentByCode($method);  // string
-                $method_arr = $this->getShipmentByCode($method) . ( $this->getSaturdayDelivery() ? ' Saturday' : '' );  // string
-                $rate->setMethodTitle($method_arr);
-                $rate->setCost($costArr[$method]);
-                $rate->setPrice($price);
-
-                $result->append($rate);
-            }
-        }
-        return $result;
-    }
-
-    /**
      * Get xml rates
      *
      * @return Mage_Shipping_Model_Rate_Result
@@ -280,6 +175,111 @@ XMLRequest;
         }
 
         return $this->_parseXmlResponse($xmlResponse);
+    }
+
+    /**
+     * Prepare shipping rate result based on response
+     *
+     * @param mixed $response
+     * @return Mage_Shipping_Model_Rate_Result
+     */
+    protected function _parseXmlResponse($xmlResponse)
+    {
+        $costArr = array();
+        $priceArr = array();
+        if (strlen(trim($xmlResponse))>0) {
+            $xml = new Varien_Simplexml_Config();
+            $xml->loadString($xmlResponse);
+            $arr = $xml->getXpath("//RatingServiceSelectionResponse/Response/ResponseStatusCode/text()");
+            $success = (int)$arr[0];
+            if ($success===1) {
+                $arr = $xml->getXpath("//RatingServiceSelectionResponse/RatedShipment");
+                $allowedMethods = explode(",", $this->getConfigData('allowed_methods'));
+
+                // Negotiated rates
+                $negotiatedArr = $xml->getXpath("//RatingServiceSelectionResponse/RatedShipment/NegotiatedRates");
+                $negotiatedActive = $this->getConfigFlag('negotiated_active')
+                    && $this->getConfigData('shipper_number')
+                    && !empty($negotiatedArr);
+
+                $allowedCurrencies = Mage::getModel('directory/currency')->getConfigAllowCurrencies();
+
+                foreach ($arr as $shipElement){
+                    $code = (string)$shipElement->Service->Code;
+                    if (in_array($code, $allowedMethods)) {
+
+                        if ($negotiatedActive) {
+                            $cost = $shipElement->NegotiatedRates->NetSummaryCharges->GrandTotal->MonetaryValue;
+                        } else {
+                            $cost = $shipElement->TotalCharges->MonetaryValue;
+                        }
+
+                        //convert price with Origin country currency code to base currency code
+                        $successConversion = true;
+                        $responseCurrencyCode = (string) $shipElement->TotalCharges->CurrencyCode;
+                        if ($responseCurrencyCode) {
+                            if (in_array($responseCurrencyCode, $allowedCurrencies)) {
+                                $cost = (float) $cost * $this->_getBaseCurrencyRate($responseCurrencyCode);
+                            } else {
+                                $errorTitle = Mage::helper('directory')->__('Can\'t convert rate from "%s-%s".', $responseCurrencyCode, $this->_request->getPackageCurrency()->getCode());
+                                $error = Mage::getModel('shipping/rate_result_error');
+                                $error->setCarrier('ups');
+                                $error->setCarrierTitle($this->getConfigData('title'));
+                                $error->setErrorMessage($errorTitle);
+                                $successConversion = false;
+                            }
+                        }
+
+                        if ($successConversion) {
+                            $costArr[$code] = $cost;
+                            $priceArr[$code] = $this->getMethodPrice(floatval($cost),$code);
+                        }
+                    }
+                }
+            } else {
+                $arr = $xml->getXpath("//RatingServiceSelectionResponse/Response/Error/ErrorDescription/text()");
+                $errorTitle = (string)$arr[0][0];
+                $error = Mage::getModel('shipping/rate_result_error');
+                $error->setCarrier('ups');
+                $error->setCarrierTitle($this->getConfigData('title'));
+                $error->setErrorMessage($this->getConfigData('specificerrmsg'));
+            }
+        }
+        $result = Mage::getModel('shipping/rate_result');
+        $defaults = $this->getDefaults();
+        if (empty($priceArr)) {
+            $error = Mage::getModel('shipping/rate_result_error');
+            $error->setCarrier('ups');
+            $error->setCarrierTitle($this->getConfigData('title'));
+            if(!isset($errorTitle)){
+                $errorTitle = Mage::helper('usa')->__('Cannot retrieve shipping rates');
+            }
+            $error->setErrorMessage($this->getConfigData('specificerrmsg'));
+            $result->append($error);
+        } else {
+            if( $this->getSaturdayDelivery() ) {
+                $saturdayShippingMethods = explode( ",", Mage::getStoreConfig('shipping/saturday_delivery_ups/allowed_methods') );
+            }
+            foreach ($priceArr as $method=>$price) {
+                if( $this->getSaturdayDelivery() && !in_array( $method, $saturdayShippingMethods ) ) {
+                    continue;
+                }
+
+                $rate = Mage::getModel('shipping/rate_result_method');
+                $rate->setCarrier('ups');
+                $rate->setCarrierTitle($this->getConfigData('title'));
+                // $rate->setMethod($method);
+                $rate->setMethod( $method . ( $this->getSaturdayDelivery() ? '_sat' : '' ) );
+                // $method_arr = $this->getShipmentByCode($method);  // string
+                $method_arr = $this->getShipmentByCode($method) . ( $this->getSaturdayDelivery() ? ' Saturday' : '' );  // string
+                $rate->setMethodTitle($method_arr);
+                $rate->setCost($costArr[$method]);
+                $rate->setPrice($price);
+
+                $result->append($rate);
+            }
+        }
+        return $result;
     }
 
 }
